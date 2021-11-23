c4ce1001f96c03a43f00d477b90997aecf120c5f
Keon <kwk236@gmail.com>
2021-11-23 09:50:26 +0900

    add dataframe & ml

---
+++ 01-spark/graphframes.py
@@ -0,0 +1,29 @@
+# https://graphframes.github.io/graphframes/docs/_site/quick-start.html
+
+# Create a Vertex DataFrame with unique ID column "id"
+v = sqlContext.createDataFrame([
+  ("a", "Alice", 34),
+  ("b", "Bob", 36),
+  ("c", "Charlie", 30),
+], ["id", "name", "age"])
+# Create an Edge DataFrame with "src" and "dst" columns
+e = sqlContext.createDataFrame([
+  ("a", "b", "friend"),
+  ("b", "c", "follow"),
+  ("c", "b", "follow"),
+], ["src", "dst", "relationship"])
+# Create a GraphFrame
+from graphframes import *
+g = GraphFrame(v, e)
+
+
+
+# Query: Get in-degree of each vertex.
+g.inDegrees.show()
+
+# Query: Count the number of "follow" connections in the graph.
+g.edges.filter("relationship = 'follow'").count()
+
+# Run PageRank algorithm, and show results.
+results = g.pageRank(resetProbability=0.01, maxIter=20)
+results.vertices.select("id", "pagerank").show()
