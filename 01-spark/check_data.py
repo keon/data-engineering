e595b362703bf801a5cd9a795ebbb3cafe30bd63
HKYoo95 <gnlrms12@gmail.com>
2023-02-22 16:13:44 +0900

    study

--- 01-spark/check_data.py
+++ 01-spark/check_data.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import pyarrow.parquet as parquet
 
-df=pd.read_csv('./data/fhvhv_tripdata_2020-03.csv')
+df=pd.read_parquet('./data/fhvhv_tripdata_2020-03.parquet')
 print(df.head(5))
